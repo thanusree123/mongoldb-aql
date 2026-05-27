1b5d6d7a58a6099042c3a20875c629d06bef4a3e
thanusree123 <gollathanusree0@gmail.com>
2026-05-27 20:22:29 +0530

    javascript

---
+++ app.js
@@ -0,0 +1,93 @@
+// function checkDiscount(isMember) {
+//     let discount = 0; 
+//     if (isMember) {
+//         discount = 20; 
+//     }
+//     console.log("Your discount is:", discount); 
+// }
+// console.log("--- Testing Member ---");
+// checkDiscount(true);  
+// console.log("--- Testing Non-Member ---");
+// checkDiscount(false);
+
+// variables 
+// const pi=1.2345;
+// let currentscore=15;
+// currentscore=25;
+// {
+//     let backgroud="hidden";
+//     var global="everywhere";
+// }
+// console.log(global);
+
+// data types
+// const developername="alex"
+// const experenice=5
+// const isBackendDeveloper=true
+// let futureproject
+// const emptybaseconnection=null
+
+// const developerprofile={
+//     name:"alex",
+//     age:28,
+//     role:"fullstack engineering "
+// }
+// console.log(developerprofile)
+// console.log(developername)
+// console.log(experenice)
+// console.log(emptybaseconnection)
+
+// operations
+// let total=10+5
+// let diff=10-5
+// let product=10*5
+// let quotient=10/2
+// let remainder=10%2
+// console.log(total)
+// console.log(diff)
+// console.log(product)
+// console.log(quotient)
+// console.log(remainder)
+
+// comparison operations
+// console.log(5=="5")
+// console.log(5==="5")
+// console.log(10>=5)
+// console.log(10!=5)
+
+
+// logical operations
+// var password=false
+// var confirmpassword=true
+// const wrong =password&&confirmpassword
+// const right=password||confirmpassword
+// console.log(wrong)
+// console.log(right)
+
+// control & statements
+// const accountbalance=500
+// const itemcost=1200
+// if(accountbalance>=itemcost){
+//     console.log("purchase approved")
+// }else if(accountbalance+1000>=itemcost){
+//     console.log("purchase approved using overdraft")
+// }else{
+//     console.log("inefficent funds")
+// }
+
+// switch statements
+const user="customer"
+switch(user){
+    case "admin":
+    console.log("full access")
+    break
+    case "editor":
+    console.log("content modification")
+    break
+    default:
+        console.log("no access")
+}
+
+
+
+
